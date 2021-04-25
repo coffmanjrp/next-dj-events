57bdc6652e8ca27ee45a1d204d2a01206102d402
coffmanjrp <coffmanjrp@gmail.com>
2021-04-24 17:02:06 -0700

    Add Header and Footer

---
+++ frontend/components/Footer.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import styles from '../styles/Footer.module.css';
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <p>Copyright &copy; DJ Events {year}</p>
+      <p>
+        <Link href="/about">About This Project</Link>
+      </p>
+    </footer>
+  );
+}
