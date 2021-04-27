f14cc96aeb762baaa1de202fc3f2bb003f51ae95
coffmanjrp <coffmanjrp@gmail.com>
2021-04-27 07:15:30 -0700

    Impliment authentication

--- frontend/pages/account/login.jsx
+++ frontend/pages/account/login.jsx
@@ -4,15 +4,23 @@ import { FaUser } from 'react-icons/fa';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Layout from '@/components/Layout';
+import AuthContext from '@/context/AuthContext';
 import styles from '@/styles/AuthForm.module.css';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { login, error } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+    }
+  }, [error]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ email, password });
+    login({ email, password });
 
     setEmail('');
     setPassword('');
--- frontend/pages/account/register.jsx
+++ frontend/pages/account/register.jsx
@@ -4,6 +4,7 @@ import { FaUser } from 'react-icons/fa';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Layout from '@/components/Layout';
+import AuthContext from '@/context/AuthContext';
 import styles from '@/styles/AuthForm.module.css';
 
 export default function RegisterPage() {
@@ -11,6 +12,13 @@ export default function RegisterPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
+  const { register, error } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+    }
+  }, [error]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,7 +27,7 @@ export default function RegisterPage() {
       return;
     }
 
-    console.log({ username, email, password, passwordConfirm });
+    register({ username, email, password, passwordConfirm });
 
     setUsername('');
     setEmail('');
@@ -71,7 +79,7 @@ export default function RegisterPage() {
               onChange={(e) => setPasswordConfirm(e.target.value)}
             />
           </div>
-          <input type="submit" value="Login" className="btn" />
+          <input type="submit" value="Register" className="btn" />
         </form>
         <p>
           Already have an account?{' '}
